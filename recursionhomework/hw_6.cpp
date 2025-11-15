5de5849d7add6d1b6b04d938cfcc57a0f87fd4c8
Sakurakouji Runa <256862649@qq.com>
2025-11-15 14:44:05 +0800

    fix homework

---
+++ recursionhomework/hw_6.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int res=0;
+ 
+int ismax(vector <int> arr,int index,int n){
+    if(res<arr[index]) res = arr[index];
+    if(index>n-1) return res;
+    else return ismax(arr,index+1,n);
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){  
+        scanf("%d",&arr[i]);
+    }
+    printf("%d",ismax(arr,0,n));
+    return 0;
+}
