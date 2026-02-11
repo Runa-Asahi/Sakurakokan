7f4bda58548b9d243c3c5e868217e7d658a282b8
Sakurakouji Runa <256862649@qq.com>
2026-02-11 23:08:00 +0800

    Change

--- Someprojects/Firstone.cpp
+++ Someprojects/Firstone.cpp
@@ -1,62 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-const int N = 1e5+10;
-int a[N];
-ll prefix[N];
-
-int find(int x){
-    if(x==0) return 2;
-    int i=1;
-    while(1){
-        if(pow(2,i)>x && pow(2,i-1)<=x){
-            return pow(2,i);
-        }
-        i++;
-    }
-}
-
+vector<ll> sig;
 int main(){
-    ios::sync_with_stdio(false); cin.tie(0);
-    int T;cin>>T;
-    while(T--){
-        int n;cin>>n;
-        for(int i=1;i<=n;i++){
-            cin>>a[i];
-        }
-        for(int i=1;i<=n;i++){
-            prefix[i] = prefix[i-1]+a[i];
-        }
-
-        long long ans = 0;
-        for(int i=1;i<=n;i++){
-            int ii = i;
-            ll need_sum = find(a[i]);
-            
-            ll score = log2(need_sum); 
-
-            while(ii <= n){
-
-                ll val = need_sum + prefix[i-1];
-                
-                auto ptr = lower_bound(prefix+ii,prefix+n+1,val);
-                
-                int j = ptr - prefix - 1;
-                
-                ll count = j - ii + 1;
-                
-                if(count > 0){
-                    ll sum_j = (ll)(ii + j) * count / 2;
-                    ll total = (ll)i * count + sum_j;
-                    
-                    ans += total * score;
-                }
-                
-                ii = j + 1;
-                need_sum *= 2;
-                score++; 
-            }
-        }
-        cout<<ans<<"\n";
-    }
-}
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	ll L;int N,K;
+	cin>>L>>N>>K;
+	for(int i=0;i<N;i++){
+		ll num;cin>>num;
+		sig.push_back(num);
+	}
+	ll left=1,right=L;
+	while(left<=right){
+		ll mid = left+(right-left)/2;//两点最大距离
+		//两点最大距离增大，放置路标的数量减小 
+		int cnt=0;ll last=sig[0];
+		for(int i=1;i<N;i++){
+			ll last=sig[i-1];
+			ll len = sig[i]-last;
+			if(len>mid){
+				cnt += (len-1)/mid;//放置路标数量 	
+			}
+		}
+		if(cnt<=K) right = mid-1;
+		else left = mid+1;
+	}
+	cout<<right+1<<'\n';
+} 
